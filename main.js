ec9dafc84784d8bd6e27f3bae9dcd35da3be41a4
Leonid <golani04@gmail.com>
2016-03-25 13:35:57 +0300

    Last version

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var SpacebookApp = function () {
   var posts = [
     {
@@ -25,7 +24,12 @@ var SpacebookApp = function () {
   var $posts = $('.posts');
 
   var createPost = function (text) {
-    posts.push({ text: text });
+    var post = {
+      text: text,
+      id: posts.length,
+      comments: []
+    };
+    posts.push(post);
   }
 
   var renderPosts = function () {
@@ -36,10 +40,10 @@ var SpacebookApp = function () {
 
       var commentsContainer = '<div class="comments-container">' + '<div class="comments-list"></div>' +
       '<input type="text" class="comment-name">' +
-      '<button class="btn btn-primary add-comment">Post Comment</button> </div>';
+      '<button class="btn btn-primary add-comment"><i class="fa fa-plus"></i></button> </div>';
 
       $posts.append('<div class="post">'
-        + '<a href="#" class="remove">remove</a> ' + '<a href="#" class="show-comments">comments</a> ' + post.text +
+        + '<a href="#" class="remove"><i class="fa fa-times"></i></a> ' + '<a href="#" class="show-comments"><i class="fa fa-comments"></i></a> ' + post.text +
         commentsContainer + '</div>');
     }
   }
@@ -48,16 +52,16 @@ var SpacebookApp = function () {
     var $clickedPost = $(currentPost).closest('.post');
 
     var index = $clickedPost.index();
-
     posts.splice(index, 1);
     $clickedPost.remove();
   }
 
-  var createComment = function (text, currentPost) {
+  var createComment = function (text, idx) {
      var comment = {
         text: text
      };
-     var idx = $(currentPost).closest('.post').index();
+
+     console.log(posts);
      posts[idx].comments.push(comment);
   };
 
@@ -68,7 +72,7 @@ var SpacebookApp = function () {
 
     $(posts).map(function (idx) {
       $(this.comments).map(function () {
-        $('.comments-list').eq(idx).append('<p>' + this.text + '<button class="btn btn-danger remove-comment">Remove</button></p>');
+        $('.comments-list').eq(idx).append('<p>' + this.text + '<button class="btn btn-danger remove-comment"><i class="fa fa-times"></i></a></button></p>');
       });
     });
   };
@@ -76,7 +80,6 @@ var SpacebookApp = function () {
   var removeComment  = function (currentPost) {
      var index = $(currentPost).closest('.post').index();
      var comment_idx = $(currentPost).parent().index();
-    //  console.log(index, comment_idx);
      posts[index].comments.splice(comment_idx, 1);
      $(currentPost).parent().remove();
   };
@@ -109,7 +112,7 @@ $('.add-post').on('click', function (e) {
 
   var text = $('#post-name').val();
   app.createPost(text);
-
+  app.renderPosts();
 });
 
 $('.posts').on('click', '.remove', function (e) {
@@ -122,7 +125,7 @@ $('.posts').on('click', '.add-comment', function (e) {
    var idx = $(this).closest('.post').index();
    var text = $('.comment-name').eq(idx).val();
 
-   app.createComment(text, this);
+   app.createComment(text, idx);
    app.renderComments();
 });
 
@@ -141,99 +144,3 @@ $('.posts').on('click', '.show-comments', function (e) {
       app.toggleComments(this);
 
 });
-=======
-(function ($) {
-   var posts = [];
-
-   var storage = {
-      // localStorage["posts4Save"] = JSON.stringify(posts);
-       // localStorage["posts4Save"] =  stored.push(JSON.stringify(posts));
-   }
-
-   // adding new post
-   var addPost = function (input, id) {
-      // initiate post
-      var post = {
-         text: input,
-         id: id,
-         comments: []
-      };
-      posts.push(post);
-   };
-
-   function updatePost () {
-      // $('.posts').find('ul').empty();
-      getPostsToHtml(posts);
-   }
-
-   function getPostsToHtml (data) {
-      var $li = $('<li></li>');
-      $('.posts').find('ul').append($li);
-
-      $.each(data, function(i, v) {
-         $li.addClass('post').attr('data-id', v.id);
-         // $li.append($('<a href="#" class="remove">remove</a>')).text(v.text);
-         $li.html('<a href="#" class="remove"><i class="fa fa-times"></i></a>' + v.text);
-         $li.append('<a href="#" class="add-comment"><i class="fa fa-comment"></i></a>' );
-      });
-   }
-
-   function writeComment($userName, $input, $submit, id) {
-      var comment = {
-         user: '',
-         text: '',
-      }
-
-      $submit.on('click', function () {
-         comment.user =  $userName.val();
-         comment.text =  $input.val();
-         $input.parent().remove();
-      });
-      posts[id].comments.push(comment);
-   }
-
-   // add post event
-   $('.add-post').on('click', function (e) {
-      e.preventDefault();
-      var input = $('#post-name').val(),
-            id = posts.length;
-      $('#post-name').val('');
-
-      addPost(input, id);
-      updatePost();
-   });
-
-   // remove event click
-   $('ul').on('click', '.remove', function (e) {
-      e.preventDefault();
-      // removing html element
-      $(this).parent().remove();
-
-      // removing element from array
-      posts.splice($(this).parent().data().id, 1);
-   });
-
-   // add comment
-   $('ul').on('click','.add-comment', function (e) {
-      var $ul = $('<ul class="list-unstyled"></ul>'),
-            $li = $('<li></li>'),
-            $span = ('<span></span>'),
-            $form = $('<form></form>'),
-            $userName = $('<input type="text"></input>'),
-            $input = $('<input type="text"></input>'),
-            $submit = $('<input type="submit" value="Submit"></input>'),
-            id = $(this).parent().data().id;
-
-      $(this).parent().append($form,$ul);
-      $form.append($userName, $input, $submit);
-      // var comment = writeComment($userName, $input, $submit);
-      console.log($(this).parent().data().id);
-
-      writeComment($userName, $input, $submit, id);
-      // $ul.append($li.append($($span).html('User: ' + comment.user + 'writes a comment: ' + comment.text)));
-   });
-
-
-
-})(jQuery);
->>>>>>> master
