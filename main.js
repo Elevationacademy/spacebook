1156a370db96797d490ad47d808c201f17ce5887
Leonid <golani04@gmail.com>
2016-03-27 09:37:47 +0300

    Last version

--- main.js
+++ main.js
@@ -1,94 +1,146 @@
-(function ($) {
-   var posts = [];
+var SpacebookApp = function () {
+  var posts = [
+    {
+      text: "Hello world",
+      id: 0,
+      comments: [
+         { text: "Man1, this is a comment!"},
+         { text: "Man2, this is a comment!"},
+         { text: "Man3, this is a comment!"}
+      ]
+   },
+    {text: "Hello world", id: 1, comments:[
+      { text: "Man, this is a comment!"},
+      { text: "Man, this is a comment!"},
+      { text: "Man, this is a comment!"}
+    ]},
+    {text: "Hello world", id: 2, comments:[
+      { text: "Man, this is a comment!"},
+      { text: "Man, this is a comment!"},
+      { text: "Man, this is a comment!"}
+    ]}
+  ];
 
-   var storage = {
-      // localStorage["posts4Save"] = JSON.stringify(posts);
-       // localStorage["posts4Save"] =  stored.push(JSON.stringify(posts));
-   }
+  var $posts = $('.posts');
 
-   // adding new post
-   var addPost = function (input, id) {
-      // initiate post
-      var post = {
-         text: input,
-         id: id,
-         comments: []
-      };
-      posts.push(post);
-   };
+  var createPost = function (text) {
+    var post = {
+      text: text,
+      id: posts.length,
+      comments: []
+    };
+    posts.push(post);
+  }
 
-   function updatePost () {
-      // $('.posts').find('ul').empty();
-      getPostsToHtml(posts);
-   }
+  var renderPosts = function () {
+    $posts.empty();
 
-   function getPostsToHtml (data) {
-      var $li = $('<li></li>');
-      $('.posts').find('ul').append($li);
+    for (var i = 0; i < posts.length; i += 1) {
+      var post = posts[i];
 
-      $.each(data, function(i, v) {
-         $li.addClass('post').attr('data-id', v.id);
-         // $li.append($('<a href="#" class="remove">remove</a>')).text(v.text);
-         $li.html('<a href="#" class="remove"><i class="fa fa-times"></i></a>' + v.text);
-         $li.append('<a href="#" class="add-comment"><i class="fa fa-comment"></i></a>' );
+      var commentsContainer = '<div class="comments-container">' + '<div class="comments-list"></div>' +
+      '<input type="text" class="comment-name">' +
+      '<button class="btn btn-primary add-comment"><i class="fa fa-plus"></i></button> </div>';
+
+      $posts.append('<div class="post">'
+        + '<a href="#" class="remove"><i class="fa fa-times"></i></a> ' + '<a href="#" class="show-comments"><i class="fa fa-comments"></i></a> ' + post.text +
+        commentsContainer + '</div>');
+    }
+  }
+
+  var removePost = function (currentPost) {
+    var $clickedPost = $(currentPost).closest('.post');
+
+    var index = $clickedPost.index();
+    posts.splice(index, 1);
+    $clickedPost.remove();
+  }
+
+  var createComment = function (text, idx) {
+     var comment = {
+        text: text
+     };
+
+     console.log(posts);
+     posts[idx].comments.push(comment);
+  };
+
+
+   var renderComments = function () {
+    // removing elements and start rendering anew each element again
+    $('.comments-list').empty();
+
+    $(posts).map(function (idx) {
+      $(this.comments).map(function () {
+        $('.comments-list').eq(idx).append('<p>' + this.text + '<button class="btn btn-danger remove-comment"><i class="fa fa-times"></i></a></button></p>');
       });
-   }
+    });
+  };
 
-   function writeComment($userName, $input, $submit, id) {
-      var comment = {
-         user: '',
-         text: '',
-      }
+  var removeComment  = function (currentPost) {
+     var index = $(currentPost).closest('.post').index();
+     var comment_idx = $(currentPost).parent().index();
+     posts[index].comments.splice(comment_idx, 1);
+     $(currentPost).parent().remove();
+  };
 
-      $submit.on('click', function () {
-         comment.user =  $userName.val();
-         comment.text =  $input.val();
-         $input.parent().remove();
-      });
-      posts[id].comments.push(comment);
-   }
+  var toggleComments = function (currentPost) {
+    var $clickedPost = $(currentPost).closest('.post');
+    $clickedPost.find('.comments-container').toggleClass('show');
+  }
 
-   // add post event
-   $('.add-post').on('click', function (e) {
+  return {
+     createPost: createPost,
+     renderPosts: renderPosts,
+     removePost: removePost,
+     createComment: createComment,
+     renderComments: renderComments,
+     removeComment: removeComment,
+     toggleComments: toggleComments
+  }
+}
+
+var app = SpacebookApp();
+
+// immediately invoke the render method
+app.renderPosts();
+app.renderComments();
+
+// Events
+$('.add-post').on('click', function (e) {
+  e.preventDefault();
+
+  var text = $('#post-name').val();
+  app.createPost(text);
+  app.renderPosts();
+});
+
+$('.posts').on('click', '.remove', function (e) {
+   e.preventDefault();
+  app.removePost(this);
+});
+
+$('.posts').on('click', '.add-comment', function (e) {
+   e.preventDefault();
+   var idx = $(this).closest('.post').index();
+   var text = $('.comment-name').eq(idx).val();
+
+   app.createComment(text, idx);
+   app.renderComments();
+});
+
+$('.posts').on('click', '.remove', function (e) {
       e.preventDefault();
-      var input = $('#post-name').val(),
-            id = posts.length;
-      $('#post-name').val('');
+      app.removePost(this);
+});
 
-      addPost(input, id);
-      updatePost();
-   });
-
-   // remove event click
-   $('ul').on('click', '.remove', function (e) {
+$('.posts').on( 'click', '.remove-comment', function (e) {
       e.preventDefault();
-      // removing html element
-      $(this).parent().remove();
+      app.removeComment(this);
+});
 
-      // removing element from array
-      posts.splice($(this).parent().data().id, 1);
-   });
+$('.posts').on('click', '.show-comments', function (e) {
+      e.preventDefault();
+      app.toggleComments(this);
 
-   // add comment
-   $('ul').on('click','.add-comment', function (e) {
-      var $ul = $('<ul class="list-unstyled"></ul>'),
-            $li = $('<li></li>'),
-            $span = ('<span></span>'),
-            $form = $('<form></form>'),
-            $userName = $('<input type="text"></input>'),
-            $input = $('<input type="text"></input>'),
-            $submit = $('<input type="submit" value="Submit"></input>'),
-            id = $(this).parent().data().id;
-
-      $(this).parent().append($form,$ul);
-      $form.append($userName, $input, $submit);
-      // var comment = writeComment($userName, $input, $submit);
-      console.log($(this).parent().data().id);
-
-      writeComment($userName, $input, $submit, id);
-      $ul.append($li.append($($span).html('User: ' + posts[id]comments.user + 'writes a comment: ' + posts[id].comments.text)));
-   });
-
-
-
-})(jQuery);
+});
